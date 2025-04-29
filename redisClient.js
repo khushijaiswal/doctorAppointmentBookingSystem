4ea0aff25db6bfcc5a6ae9d792501223a5d3c7da
khushijaiswal <jaiswalkhushi9011@gmail.com>
2025-04-29 10:11:59 +0530

    redis url change

--- redisClient.js
+++ redisClient.js
@@ -1,7 +1,8 @@
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    url: 'redis://localhost:6379'
+    // url: 'redis://localhost:6379'
+    url: '127.0.0.1:6379'
 });
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
